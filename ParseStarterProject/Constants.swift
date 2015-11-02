fc3472e8b014e5ab886eca898fb88e007e6412d8
heidiyee <heidi.n.yee@gmail.com>
2015-11-02 13:26:24 -0800

    Created a new starter project through Parse

---
+++ ParseStarterProject/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Heidi Yee on 11/2/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import Foundation
+
+let kParseApplicationId = “CxF7R0XRDhhioyTnQwkIxcC0TcUmZJngRbhy6YBk”
+let kParseApplicationClientKey = “3XvTQ2csex5RINLaaOBmrS9aK1u5wDYA1B7jxA1W”
